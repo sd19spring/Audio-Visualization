9bd5c3fc49f023e2516487cd986106d87a41601b
David Tarazi <dtarazi@olin.edu>
2019-04-19 12:25:05 -0400

    worked with the visualizer class and getting data

--- visualizer.py
+++ visualizer.py
@@ -5,8 +5,11 @@ import random
 import pygame
 from pygame.locals import *
 
-from shapes import *
-from spotify_data import beats, bars, sections
+from shape_classes import *
+from spotify_data import *
+
+data = {}
+data['beats'], data['bars'], data['danceability'], data['loudness'], data['energy'], data['tempo'], data['mood'] = collect_data()
 
 screen = {}
 screen['width'] = 832
@@ -16,18 +19,27 @@ screen['window'] = pygame.display.set_mode( [screen['width'], screen['height']],
 WHITE = (255,255,255)
 BLACK = (0, 0, 0)
 
+
 class Display:
 	"""
 	The App class is where all the objects are created and the mechanics of the
 	game is run.
 	"""
-	def __init__(self, beats, bars, sections):
+	def __init__(self):
 		"""
 		Creates basic initialization for the variables, clock, etc.
 		"""
         self.numshapes = #some form of tempo input
         self.beat_stamps = [] #list of beat timestamps
+		#adds the start time of all the beats to the beat_stamps list
+		for i in data['beats']:
+			self.beat_stamps.append(i['start'])
+
         self.bar_stamps = [] #list of bar timestamps
+		#adds the start time of all the beats to the beat_stamps list
+		for i in data['bars']:
+			self.bar_stamps.append(i['start'])
+
         self.colors = [] #some sort of list of colors based on mood info
         self.movement_speeds = [] #list of speeds based on tempo
         #fill shapes with instances of random shapes
