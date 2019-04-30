f16cec55c9c9b2091aa7dfc404bb63d9a8825191
David Tarazi <dtarazi@olin.edu>
2019-04-30 12:18:44 -0400

    made the code more readable and got rid of an excess file

--- shape_classes.py
+++ shape_classes.py
@@ -25,24 +25,30 @@ class Rectangle:
 		self.yspeed = danceability*2
 		self.expand_speed = loudness/random.randint(20,40)
 
-	def update(self, type = "floaty"):
+	def update(self, type):
 		"""
 		Every frame this method will add the speed to the object's coordinates to make the object move
 		"""
 		if type == "floaty":
-			self.x += self.xspeed
-			self.y += self.yspeed
+			self.update_floaty()
 
-			#Defines how a rectange leaves and re-enters the screen
-			if self.xspeed > 0 and (self.x + self.width) > screen['width']:
-				self.x = -self.width
-			elif self.xspeed < 0 and (self.x) < -self.width:
-				self.x = screen['width']
+	def update_floaty(self):
+		"""
+		Updates the screen with the floaty style
+		"""
+		self.x += self.xspeed
+		self.y += self.yspeed
 
-			if self.yspeed > 0 and (self.y + self.height) > screen['height']:
-				self.y = -self.height
-			elif self.xspeed < 0 and (self.y) < -self.width:
-				self.x = screen['height']
+		#Defines how a rectange leaves and re-enters the screen
+		if self.xspeed > 0 and (self.x + self.width) > screen['width']:
+			self.x = -self.width
+		elif self.xspeed < 0 and (self.x) < -self.width:
+			self.x = screen['width']
+
+		if self.yspeed > 0 and (self.y + self.height) > screen['height']:
+			self.y = -self.height
+		elif self.xspeed < 0 and (self.y) < -self.width:
+			self.x = screen['height']
 
 	def move_to_random(self):
 		"""
@@ -75,36 +81,49 @@ class Circle:
         self.yspeed = danceability*2
         self.expand_speed = loudness/random.randint(20,40)
 
-    def update(self, type = "floaty"):
+    def update(self, type):
         """
 		Every frame it will add the speed to the object to look like motion
+		based on which style type is being implemented
 		"""
         if type == "floaty":
-            self.x += self.xspeed
-            self.y += self.yspeed
-
-			#defines how it leaves screen and reappears
-            if self.xspeed > 0 and (self.x + self.radius) > screen['width']:
-                self.x = -self.radius
-            elif self.xspeed < 0 and (self.x) < -self.radius:
-                self.x = screen['width']
-
-            if self.yspeed > 0 and (self.y + self.radius) > screen['height']:
-                self.y = -self.radius
-            elif self.xspeed < 0 and (self.y) < -self.radius:
-                self.x = screen['height']
+			self.update_floaty()
 
         elif type == "bubbles":
-            self.radius += self.expand_speed
+            self.update_bubbles()
 
-			#if the circle is too small reset its location and size
-            if self.radius < 5:
-                self.move_to_random()
-                self.radius = 50
-			#if the circle is too big reset it
-            if self.radius > 200:
-                self.move_to_random()
-                self.radius = 100
+	def update_floaty(self):
+		"""
+		Updates the screen with the floaty style
+		"""
+		self.x += self.xspeed
+		self.y += self.yspeed
+
+		#defines how it leaves screen and reappears
+		if self.xspeed > 0 and (self.x + self.radius) > screen['width']:
+			self.x = -self.radius
+		elif self.xspeed < 0 and (self.x) < -self.radius:
+			self.x = screen['width']
+
+		if self.yspeed > 0 and (self.y + self.radius) > screen['height']:
+			self.y = -self.radius
+		elif self.xspeed < 0 and (self.y) < -self.radius:
+			self.x = screen['height']
+
+	def update_bubbles(self):
+		"""
+		Updates the screen with the bubbles style
+		"""
+		self.radius += self.expand_speed
+
+		#if the circle is too small reset its location and size
+		if self.radius < 5:
+			self.move_to_random()
+			self.radius = 50
+		#if the circle is too big reset it
+		if self.radius > 200:
+			self.move_to_random()
+			self.radius = 100
 
     def move_to_random(self):
         """
@@ -119,23 +138,3 @@ class Circle:
         Draws the circle on the screen
         """
         pygame.draw.circle(screen['window'], self.color, [int(self.x), int(self.y)], int(self.radius))
-
-"""
-class Style(numshapes):
-	"""
-	Creates a generic style class with shapes that can be applied to all other
-	styles.
-	"""
-
-class Floaty(Style):
-	"""
-	Creates a Floaty style that adds additional object parameters for this
-	specific style and contains an update function specific to the floaty style
-	"""
-
-class Bubbles(Style):
-	"""
-	Creates a Bubbles style that adds additional object parameters for this
-	specific style and contains an update function specific to the bubbles style
-	"""
-"""
